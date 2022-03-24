2c71a3d7c4596c937dfc76ba64bc48256898119f
WWWeaver <wwweaver@mtu.edu>
2022-03-24 14:03:27 -0400

    Initial simple MATLAB script to plot a sine wave.  Used to learn basics of GIT.

---
+++ SimplePlotScript.m
@@ -0,0 +1,23 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% WWeaver
+% 3/24/2022
+%
+% MATLAB Script to learn GIT
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear
+clc
+close all
+
+%%
+
+t=0:0.001:1;
+
+figure
+plot(t,sin(2*pi*10*t))
+ grid on
+ ylabel('Y')
+ xlabel('t')
+ title('My Plot')
+
+
